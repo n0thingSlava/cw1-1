53775ad2ef9ea8a0e3961c217fac1269be7c2dbc
Slav1k <maznevslava@yandex.ru>
2022-08-30 19:44:26 +0500

    dz

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,30 @@
 public class Employee {
+    private String FIO;
+    private int Department;
+    private float Salary;
+    private static int Counter;
+
+    private int Id;
+    public int getId() {
+        return this.Id; }
+    public String getFIO() {
+        return this.FIO; }
+    public int getDepartment() {
+        return this.Department; }
+    public float getSalary() {
+        return this.Salary; }
+
+    public Employee(String fio, int dept, float salary) {
+        FIO = fio;
+        Department = dept;
+        Salary = salary;
+        Id = ++Counter;
+
+    }
+    @Override
+    public String toString() {
+        return "Id: " + Id + " Fio: " + FIO + " Dept: " + Department + " Salary: " + Salary;
+
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee[] empl = new Employee[10];
+        empl[0] = new Employee("Ivanov Ivan Ivanovich1", 1, 75500f);
+        empl[1] = new Employee("Ivanov Ivan Ivanovich2", 2, 83450f);
+        empl[2] = new Employee("Ivanov Ivan Ivanovich3", 3, 71200f);
+        empl[3] = new Employee("Ivanov Ivan Ivanovich4", 4, 90200f);
+        empl[4] = new Employee("Ivanov Ivan Ivanovich5", 5, 99200f);
+        empl[5] = new Employee("Ivanov Ivan Ivanovich5", 6, 99200f);
+        empl[6] = new Employee("Ivanov Ivan Ivanovich5", 7, 99200f);
+        empl[7] = new Employee("Ivanov Ivan Ivanovich5", 8, 99200f);
+        empl[8] = new Employee("Ivanov Ivan Ivanovich5", 9, 99200f);
+        empl[9] = new Employee("Ivanov Ivan Ivanovich10", 10, 99200f);
+
+        for (Employee e : empl) {
+            System.out.println(e);
+        }
+        int sum = 0;
+        int i;
+        for (i = 0; i < empl.length; i++) {
+            sum += empl[i].getSalary();
+        }
+        System.out.println("Cумма затрат на зарплаты в месяц-" + sum);
+        System.out.println();
+
     }
 }
