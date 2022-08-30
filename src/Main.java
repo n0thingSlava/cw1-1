52961ea9d70e05fa7d09d3597d5e666c0c6602ba
Slav1k <maznevslava@yandex.ru>
2022-08-30 19:51:59 +0500

    dz

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ public class Main {
         empl[2] = new Employee("Ivanov Ivan Ivanovich3", 3, 71200f);
         empl[3] = new Employee("Ivanov Ivan Ivanovich4", 4, 90200f);
         empl[4] = new Employee("Ivanov Ivan Ivanovich5", 5, 94200f);
-        empl[5] = new Employee("Ivanov Ivan Ivanovich5", 6, 95300f);
-        empl[6] = new Employee("Ivanov Ivan Ivanovich5", 7, 96400f);
-        empl[7] = new Employee("Ivanov Ivan Ivanovich5", 8, 97500f);
-        empl[8] = new Employee("Ivanov Ivan Ivanovich5", 9, 98600f);
-        empl[9] = new Employee("Ivanov Ivan Ivanovich10", 10, 99700f);
+        empl[5] = new Employee("Ivanov Ivan Ivanovich5", 1, 95300f);
+        empl[6] = new Employee("Ivanov Ivan Ivanovich5", 2, 96400f);
+        empl[7] = new Employee("Ivanov Ivan Ivanovich5", 3, 97500f);
+        empl[8] = new Employee("Ivanov Ivan Ivanovich5", 4, 98600f);
+        empl[9] = new Employee("Ivanov Ivan Ivanovich10", 5, 99700f);
 
         for (Employee e : empl) {
             System.out.println(e);
