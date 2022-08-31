4a6920671a708e7b76a6ebe7163e043ec7e46053
Slav1k <maznevslava@yandex.ru>
2022-08-31 16:48:47 +0500

    dz

--- src/Employee.java
+++ src/Employee.java
@@ -35,22 +35,22 @@ public class Employee {
     public static void main(String[] args) {
         Employee[] employee = new Employee[10];
 
-        employee[0] = new Employee(1, ".bobych..", 1000); // и таких ровно 10
-        employee[1] = new Employee(1, ".natanich.", 2000);
-        employee[2] = new Employee(1, "..fekin.", 3000);
-        employee[3] = new Employee(1, "...gorochov", 4000);
-        employee[4] = new Employee(1, "...ivanov", 4000);
-        employee[5] = new Employee(1, "...petrov", 4000);
-        employee[6] = new Employee(1, "...nikolaev.", 4000);
-        employee[7] = new Employee(1, "..pavluchenko.", 6000);
-        employee[8] = new Employee(1, "...umarbaev", 4000);
-        employee[9] = new Employee(1, "...davinchi", 5000);
+        employee[0] = new Employee(1, "Рудольф", 1000); // и таких ровно 10
+        employee[1] = new Employee(1, "Афаыа", 2000);
+        employee[2] = new Employee(1, "Аыпвып", 3000);
+        employee[3] = new Employee(1, "Афыаыфа", 4000);
+        employee[4] = new Employee(1, "Ркрукр", 4000);
+        employee[5] = new Employee(1, "Ркуццуп", 4000);
+        employee[6] = new Employee(1, "Айпйыи", 4000);
+        employee[7] = new Employee(1, "Пиылдыв", 6000);
+        employee[8] = new Employee(1, "Млыфлафы", 4000);
+        employee[9] = new Employee(1, "АФцйдм", 5000);
 
             IntSummaryStatistics result = Arrays.stream(employee).collect(Collectors.summarizingInt(Employee::getSalary));
-            System.out.println("Сумма затрат"+ result.getSum());
-            System.out.println("Минимальная ЗП"+ result.getMin());
-            System.out.println("Максимальная ЗП" +result.getMax());
-            System.out.println(result.getAverage());
+            System.out.println("Сумма затрат - "+ result.getSum());
+            System.out.println("Минимальная ЗП - "+ result.getMin());
+            System.out.println("Максимальная ЗП - " +result.getMax());
+            System.out.println("Средняя ЗП - "+ result.getAverage());
         for (Employee e : employee) {
             System.out.println(e);
         }
