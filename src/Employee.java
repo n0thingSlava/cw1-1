1de1c2e99952c5d6b2d83be17fc86a8bf9c65260
Slav1k <maznevslava@yandex.ru>
2022-08-31 15:02:32 +0500

    dz

--- src/Employee.java
+++ src/Employee.java
@@ -2,29 +2,65 @@ public class Employee {
     private String FIO;
     private int Department;
     private float Salary;
-    private static int Counter;
 
     private int Id;
-    public int getId() {
-        return this.Id; }
+
     public String getFIO() {
-        return this.FIO; }
+        return FIO;
+    }
+
+    public void setFIO(String FIO) {
+        this.FIO = FIO;
+    }
+
     public int getDepartment() {
-        return this.Department; }
+        return Department;
+    }
+
+    public void setDepartment(int department) {
+        Department = department;
+    }
+
     public float getSalary() {
-        return this.Salary; }
+        return Salary;
+    }
+
+    public void setSalary(float salary) {
+        Salary = salary;
+    }
+
+    public int getId() {
+        return Id;
+}
 
     public Employee(String fio, int dept, float salary) {
         FIO = fio;
         Department = dept;
         Salary = salary;
-        Id = ++Counter;
+        Id ++;
 
     }
     @Override
     public String toString() {
         return "№: " + Id + " ФИО: " + FIO + " Отдел: " + Department + " ЗП: " + Salary;
 
+    }
+    public static void main(String[] args) {
+        Employee[] empl = new Employee[10];
+        empl[0] = new Employee("Ivanov Ivan Ivanovich1", 1, 75500f);
+        empl[1] = new Employee("Ivanov Ivan Ivanovich2", 2, 83450f);
+        empl[2] = new Employee("Ivanov Ivan Ivanovich3", 3, 71200f);
+        empl[3] = new Employee("Ivanov Ivan Ivanovich4", 4, 90200f);
+        empl[4] = new Employee("Ivanov Ivan Ivanovich5", 5, 94200f);
+        empl[5] = new Employee("Ivanov Ivan Ivanovich5", 1, 95300f);
+        empl[6] = new Employee("Ivanov Ivan Ivanovich5", 2, 96400f);
+        empl[7] = new Employee("Ivanov Ivan Ivanovich5", 3, 97500f);
+        empl[8] = new Employee("Ivanov Ivan Ivanovich5", 4, 98600f);
+        empl[9] = new Employee("Ivanov Ivan Ivanovich10", 5, 99700f);
+
+        for (Employee e : empl) {
+            System.out.println(e);
+        }
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,3 @@
 public class Main {
-    public static void main(String[] args) {
-        Employee[] empl = new Employee[10];
-        empl[0] = new Employee("Ivanov Ivan Ivanovich1", 1, 75500f);
-        empl[1] = new Employee("Ivanov Ivan Ivanovich2", 2, 83450f);
-        empl[2] = new Employee("Ivanov Ivan Ivanovich3", 3, 71200f);
-        empl[3] = new Employee("Ivanov Ivan Ivanovich4", 4, 90200f);
-        empl[4] = new Employee("Ivanov Ivan Ivanovich5", 5, 94200f);
-        empl[5] = new Employee("Ivanov Ivan Ivanovich5", 1, 95300f);
-        empl[6] = new Employee("Ivanov Ivan Ivanovich5", 2, 96400f);
-        empl[7] = new Employee("Ivanov Ivan Ivanovich5", 3, 97500f);
-        empl[8] = new Employee("Ivanov Ivan Ivanovich5", 4, 98600f);
-        empl[9] = new Employee("Ivanov Ivan Ivanovich10", 5, 99700f);
 
-        for (Employee e : empl) {
-            System.out.println(e);
-        }
-        int sum = 0;
-        int i;
-        for (i = 0; i < empl.length; i++) {
-            sum += empl[i].getSalary();
-        }
-        System.out.println("Cумма затрат на зарплаты в месяц-" + sum);
-        System.out.println();
-
-    }
 }
